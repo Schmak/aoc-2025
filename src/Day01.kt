ca3ba6c05294b97f6ecc375cedab2bfefb810ede
Evgeny Shmakov <evgeny.shmakov@jetbrains.com>
2025-12-16 23:45:08 +0100

    Day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,21 +1,42 @@
-fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+import java.lang.Math.floorDiv
+import kotlin.math.abs
+
+object Day01 : Task<List<Int>> {
+    const val SIZE = 100
+    override fun parse(input: List<String>): List<Int> =
+        input.map {
+            val sign = if (it[0] == 'L') -1 else 1
+            sign * it.substring(1).toInt()
+        }
+
+    fun part1(input: List<Int>): Int =
+        input.asSequence().scan(50) { acc, i -> (acc + i + SIZE) % SIZE }.count { it == 0 }
+
+    fun part2(input: List<Int>): Int =
+        input.asSequence().scan(50) { acc, i -> acc + i }
+            .zipWithNext()
+            .sumOf { (a, b) ->
+                val ad = floorDiv(a, SIZE)
+                val bd = floorDiv(b, SIZE)
+                abs(ad - bd) + when {
+                    a < b -> 0
+                    a % SIZE == 0 -> -1
+                    b % SIZE == 0 -> 1
+                    else -> 0
+                }
+            }
+
+    fun solve() {
+        val testInput = readInput("Day01_test")
+        assertThat(part1(testInput)).isEqualTo(3)
+        assertThat(part2(testInput)).isEqualTo(6)
+
+        val input = readInput("Day01")
+        assertThat(part1(input)).isEqualTo(997)
+        println(part1(input))
+        assertThat(part2(input)).isEqualTo(5978)
+        println(part2(input))
     }
-
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
-
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    // Read the input from the `src/Day01.txt` file.
-    val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
 }
+
+fun main() = Day01.solve()
--- src/Utils.kt
+++ src/Utils.kt
@@ -1,21 +1,16 @@
-import java.math.BigInteger
-import java.security.MessageDigest
 import kotlin.io.path.Path
 import kotlin.io.path.readText
 
-/**
- * Reads lines from the given input txt file.
- */
-fun readInput(name: String) = Path("src/$name.txt").readText().trim().lines()
+interface Task<INPUT> {
+    fun parse(input: List<String>): INPUT
 
-/**
- * Converts string to md5 hash.
- */
-fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray()))
-    .toString(16)
-    .padStart(32, '0')
+    fun readInput(name: String) = parse(Path("res/$name.txt").readText().trim().lines())
+}
 
-/**
- * The cleaner shorthand for printing output.
- */
-fun Any?.println() = println(this)
+data class Assertion<T>(val value: T) {
+    fun isEqualTo(other: T) {
+        check(value == other) { "Expecting value to be '$other' but was '$value'" }
+    }
+}
+
+fun <T> assertThat(obj: T) = Assertion(obj)
